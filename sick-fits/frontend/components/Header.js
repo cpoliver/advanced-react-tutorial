d7256061512f75a2871825b5bb17e9a889d7fec3
Charles Oliver <charles.oliver@casumo.com>
2018-11-11 07:40:08 +0000

    2.6 add styled components

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,18 +1,64 @@
 import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
 
 import Nav from "./Nav";
 
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+
+  a {
+    color: ${props => props.theme.black};
+    padding: 0.5rem;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+
+  @media (max-w9dth: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    align-items: stretch;
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+
+    @media (max-widthl 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid ${props => props.theme.greyLight};
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
+
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
-      <a href="">Old Crow</a>
+      <Logo>
+        <Link href="/">
+          <a>Old Crow</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,16 +1,41 @@
 import React from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 import Header from "./Header";
 import Meta from "./Meta";
 
+const theme = {
+  red: "tomato",
+  black: "#393939",
+  grey: "#3A3A3A",
+  greyLight: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0,.09"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  color: ${props => props.theme.black};
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  padding: 2rem;
+`;
+
 class Page extends React.Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
